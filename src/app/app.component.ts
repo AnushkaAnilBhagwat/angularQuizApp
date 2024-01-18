9fa28f927f0ee3272139a6c3d1872c1c3141c9db
Anushka Bhagwat <anushkabhagwat3@gmail.com>
2024-01-18 08:50:35 +0530

    selected options stored in array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -13,9 +14,37 @@ export class AppComponent {
   title = 'angularQuizApp';
 
   choice: any = '';
-  list = ["HTML", "CSS", "JS"];
+  choiceArray: any = [];
+  arr: MyArrayType = [
+    { id: "1",
+      question: "Which of the following is your favourite",
+      options: [{ id: "1",select: false, value: "HTML"}, { id: "2",select: false, value: "CSS"}, { id: "3",select: false, value: "JS"}],
+      correctOption: "JS"
+    },
+    {
+      id: "2",
+      question: "Which city do you belong to",
+      options: [{ id: "1", select: false, value: "Pune"}, { id: "2", select: false, value: "Mumbai"}],
+      correctOption: "Pune"
+    }
+  ];
 
-  onRadioChange(event:any){
+  onRadioChange(event:any, id1:string){
     this.choice = event.target.value;
+
+    for (let i = 0; i< this.arr.length; i++){
+      if (this.arr[i].id === id1) {
+        for (let j = 0; j < this.choiceArray.length; j++) {
+          for (let k = 0; k < this.arr[i].options.length; k++) {
+            if (this.arr[i].options[k].value === this.choiceArray[j] ) {
+              this.choiceArray.splice(j,1);
+            }
+          }
+        }
+      }
+    }
+    this.choiceArray.push(this.choice);
+    console.log(this.choiceArray);
   }
 }
+type MyArrayType = Array<{id: string, question: string, options: any, correctOption: string}>;
