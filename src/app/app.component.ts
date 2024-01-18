7339763b90506c2cc9e2e257f7f9d3841ead8cd0
Anushka Bhagwat <anushkabhagwat3@gmail.com>
2024-01-18 21:59:46 +0530

    alert result added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,19 +2,21 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angularQuizApp';
 
   choice: any = '';
   choiceArray: any = [];
+  result = 0;
   arr: MyArrayType = [
     { id: "1",
       question: "Which of the following is your favourite",
@@ -37,6 +39,7 @@ export class AppComponent {
         for (let j = 0; j < this.choiceArray.length; j++) {
           for (let k = 0; k < this.arr[i].options.length; k++) {
             if (this.arr[i].options[k].value === this.choiceArray[j] ) {
+              this.arr[i].options[k].select = false;
               this.choiceArray.splice(j,1);
             }
           }
@@ -44,7 +47,29 @@ export class AppComponent {
       }
     }
     this.choiceArray.push(this.choice);
+    for (let i = 0; i< this.arr.length; i++){
+      if (this.arr[i].id === id1) {
+        for (let k = 0; k < this.arr[i].options.length; k++) {
+          if (this.arr[i].options[k].value === this.choice) {
+            this.arr[i].options[k].select = true;
+          }
+        }
+      }
+    }
     console.log(this.choiceArray);
+  }
+
+  onSubmit(event: any) {
+    for (let i = 0; i < this.arr.length; i++) {
+      for (let j = 0; j < this.choiceArray.length; j++) {
+        if (this.arr[i].correctOption === this.choiceArray[j]) {
+          this.result++;
+          this.choiceArray.splice(j,1);
+        }
+      }
+    }
+    Swal.fire("Quiz Submitted Successfuly!", "Your Score - "+ this.result.toString(), "success");
+
   }
 }
 type MyArrayType = Array<{id: string, question: string, options: any, correctOption: string}>;
