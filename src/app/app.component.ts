0c43d5b4392000816179320e22bc8431df32d89f
Anushka Bhagwat <anushkabhagwat3@gmail.com>
2024-01-18 23:14:27 +0530

    added css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,15 +19,105 @@ export class AppComponent {
   result = 0;
   arr: MyArrayType = [
     { id: "1",
-      question: "Which of the following is your favourite",
-      options: [{ id: "1",select: false, value: "HTML"}, { id: "2",select: false, value: "CSS"}, { id: "3",select: false, value: "JS"}],
-      correctOption: "JS"
+      question: "Angular is primarily considered as?",
+      options: [
+      { id: "1",select: false, value: "JavaScript Framework"},
+      { id: "2",select: false, value: "CSS Framework"},
+      { id: "3",select: false, value: "Content management system"},
+      { id: "4",select: false, value: "Database System"}
+      ],
+      correctOption: "JavaScript Framework"
+    },
+    { id: "2",
+      question: "Which of the following is a core component in an Angular application?",
+      options: [
+      { id: "1",select: false, value: "ViewController"},
+      { id: "2",select: false, value: "Directive"},
+      { id: "3",select: false, value: "Activity"},
+      { id: "4",select: false, value: "Observer"}
+      ],
+      correctOption: "Directive"
+    },
+    { id: "3",
+      question: "How do you bind data to an attribute in Angular?",
+      options: [
+      { id: "1",select: false, value: "{{variable}}"},
+      { id: "2",select: false, value: "(variable)"},
+      { id: "3",select: false, value: "[variable]"},
+      { id: "4",select: false, value: "=variable="}
+      ],
+      correctOption: "[variable]"
+    },
+    { id: "4",
+      question: "Which Angular decorator is used to listen to DOM events?",
+      options: [
+      { id: "1",select: false, value: "@Output()"},
+      { id: "2",select: false, value: "@Input()"},
+      { id: "3",select: false, value: "@Event()"},
+      { id: "4",select: false, value: "@HostListener"}
+      ],
+      correctOption: "@HostListener"
+    },
+    { id: "5",
+      question: "What does a pipe do in Angular?",
+      options: [
+      { id: "1",select: false, value: "Connects two components"},
+      { id: "2",select: false, value: "Transforms data in the template"},
+      { id: "3",select: false, value: "Merges streams of data"},
+      { id: "4",select: false, value: "Opens a direct connection to the server"}
+      ],
+      correctOption: "Transforms data in the template"
+    },
+    { id: "6",
+      question: "Which decorator allows communication from a child component to its parent?",
+      options: [
+      { id: "1",select: false, value: "@Output()"},
+      { id: "2",select: false, value: "@Input()"},
+      { id: "3",select: false, value: "@ViewChild()"},
+      { id: "4",select: false, value: "@Connect()"}
+      ],
+      correctOption: "@Output()"
+    },
+    { id: "7",
+      question: "How can you generate a new service using Angular CLI?",
+      options: [
+      { id: "1",select: false, value: "ng create service my-service"},
+      { id: "2",select: false, value: "ng generate service my-service"},
+      { id: "3",select: false, value: "ng new service my-service"},
+      { id: "4",select: false, value: "ng add service my-service"}
+      ],
+      correctOption: "ng generate service my-service"
+    },
+    { id: "8",
+      question: "What is the use of Angular Directives?",
+      options: [
+      { id: "1",select: false, value: "To inject services"},
+      { id: "2",select: false, value: "To initialize component state"},
+      { id: "3",select: false, value: "To manipulate the DOM elements"},
+      { id: "4",select: false, value: "To store data"}
+      ],
+      correctOption: "To manipulate the DOM elements"
+    },
+    { id: "9",
+      question: "Which Angular decorator is used for making a class a root module?",
+      options: [
+      { id: "1",select: false, value: "@Module()"},
+      { id: "2",select: false, value: "@Component()"},
+      { id: "3",select: false, value: "@Directive()"},
+      { id: "4",select: false, value: "@Ng Module()"}
+      ],
+      correctOption: "@Ng Module()"
     },
     {
-      id: "2",
-      question: "Which city do you belong to",
-      options: [{ id: "1", select: false, value: "Pune"}, { id: "2", select: false, value: "Mumbai"}],
-      correctOption: "Pune"
+      id: "10",
+      question: "Which is the correct syntax for an Angular Event binding?",
+      options: [
+      { id: "1", select: false, value: '{click}="doSomething()"'},
+      { id: "2", select: false, value: 'on-click="doSomething()"'},
+      { id: "3",select: false, value: '(click)="doSomething()"'},
+      { id: "4",select: false, value: 'click[]="doSomething()"'}
+      ],
+      correctOption: '(click)="doSomething()"'
     }
   ];
 
